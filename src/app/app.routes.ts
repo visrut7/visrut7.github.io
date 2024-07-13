98b2a2ae8a97cf334c911f2f7ecdf4ed9ffbac2d
visrut <visrut@incubyte.co>
2024-07-13 20:06:34 +0530

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,13 @@ import { CnnComponent } from './cnn/cnn.component';
 import { RotateImageComponent } from './rotate-image/rotate-image.component';
 import { BlurBrushForImageComponent } from './blur-brush-for-image/blur-brush-for-image.component';
 import { TestRunnerComponent } from './test-runner/test-runner.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
   {
     path: 'cnn',
     component: CnnComponent,
